b6d0004d041a70c9e01072fbd3951b0e5426268f
Kateryna Strough <katerynastrough@hotmail.com>
2019-05-30 23:54:45 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,7 +10,13 @@ class CashRegister
     
   end 
   
-  
+   def add_item(title, amount, quantity=1)
+    self.total += amount * quantity
+    quantity.times do
+      items << title
+    end
+    self.last_transaction = amount * quantity
+  end
 
   
 end
