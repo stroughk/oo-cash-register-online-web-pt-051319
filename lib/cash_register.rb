813b4cbc88dd0f602cb54184f5f7883d27afb31b
Kateryna Strough <katerynastrough@hotmail.com>
2019-05-30 23:53:07 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,15 +3,11 @@ class CashRegister
   def initialize(discount=0)
     @total = 0 
     @discount=discount
-    
+    @items=[]
     
   end 
   
   
-  def discount
-    @total=20 
-
-  end
   
   total 
   
